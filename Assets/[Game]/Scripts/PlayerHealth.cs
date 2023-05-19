e8da0846e7897cbd0d9f979190a3748ab6642b17
Deniz Kaymakci <deniz.kaymakci@hotmail.com>
2023-05-19 22:19:28 +0300

    Enemy1

---
+++ Assets/[Game]/Scripts/PlayerHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxHealth = 100;
+    [SerializeField]
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        _currentHealth -= damageAmount;
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Perform actions when the player dies
+        // For example, play death animation, show game over screen, etc.
+        Debug.Log("Player died.");
+    }
+}
+
+
