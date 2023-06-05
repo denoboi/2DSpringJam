6cf04a2b103d3d68b5aa4184f7902ee71294536b
Deniz Kaymakci <deniz.kaymakci@hotmail.com>
2023-06-06 00:44:06 +0300

    New Enemy, Patrol

---
+++ Assets/[Game]/Scripts/EnemyScripts/EnemyPatrol.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+   public GameObject pointA;
+   public GameObject pointB;
+   public float _speed = 2f;
+   private Rigidbody2D _rb2d;
+   private Transform _targetPoint;
+   
+   Rigidbody2D Rb2D => _rb2d ??= GetComponent<Rigidbody2D>();
+
+   private void Start()
+   {
+      _targetPoint = pointB.transform;
+      
+   }
+   
+   private void Update()
+   {
+      Move();
+   }
+   
+   private void Move()
+   {
+
+      Vector2 point = _targetPoint.position - transform.position;
+
+      if (_targetPoint == pointB.transform)
+      {
+        Rb2D.velocity = new Vector2(_speed, 0);
+      }
+      else
+      {
+         Rb2D.velocity = new Vector2(-_speed, 0); 
+      }
+         
+
+      if (Vector2.Distance(transform.position, _targetPoint.position) < 1f && _targetPoint == pointB.transform)
+      {
+         Flip();
+         _targetPoint = pointA.transform;
+         
+      }
+      if (Vector2.Distance(transform.position, _targetPoint.position) < 1f && _targetPoint == pointA.transform) 
+      {
+         Flip();
+         _targetPoint = pointB.transform;
+         
+         
+      }
+      
+   }
+   
+   private void Flip()
+   {
+      Vector3 localScale = transform.localScale;
+      localScale.x *= -1;
+      transform.localScale = localScale;
+   }
+
+   private void OnDrawGizmos()
+   {
+      Gizmos.DrawWireSphere( pointA.transform.position, .5f);
+      Gizmos.DrawWireSphere( pointB.transform.position, .5f);
+      Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+      
+   }
+}
